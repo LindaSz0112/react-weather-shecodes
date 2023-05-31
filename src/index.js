07c63f187fb1d887b07376725ba1509599220bf0
LindaSz0112 <lindaszarvas@gmail.com>
2023-05-31 15:33:29 +0200

    included bootstrap in react and removed it from html

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.css";
+
 import "./WeatherApp.css";
 import Search from "./Search";
 import ExtraInfo from "./ExtraInfo";
