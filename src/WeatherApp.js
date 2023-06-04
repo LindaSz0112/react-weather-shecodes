90c908eac4dbe2dc88e8c5780c4badd5ddcc4b1d
LindaSz0112 <lindaszarvas@gmail.com>
2023-06-04 13:09:20 +0200

    Implement some changes regarding the API key

--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -1,47 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import "./WeatherApp.css";
+import axios from "axios";
 
 export default function WeatherApp() {
-  return (
-    <div className="WeatherApp">
-      <form className="Search">
-        <input type="text" placeholder="City" />
-        <button>Search</button>
-      </form>
-      <div className="CityDetails row">
-        <div className="city-details col">
-          <h1>Prague</h1>
-          <span>11.05.2023.</span>
-          <br />
-          <span>17:38</span>
-        </div>
-        <div className="temperature col">
-          <h2>
-            <span className="temperature">18</span>
-            <span className="temp-c">°C </span>
+  const [loaded, setLoaded] = useState(true);
 
-            <div>Clear Sky</div>
-          </h2>
+  if (loaded) {
+    return (
+      <div className="WeatherApp">
+        <form className="Search">
+          <input type="text" placeholder="City" />
+          <button>Search</button>
+        </form>
+        <div className="CityDetails row">
+          <div className="city-details col">
+            <h1>Prague</h1>
+            <span>11.05.2023.</span>
+            <br />
+            <span>17:38</span>
+          </div>
+          <div className="temperature col">
+            <h2>
+              <span className="temperature">18</span>
+              <span className="temp-c">°C </span>
+
+              <div>Clear Sky</div>
+            </h2>
+          </div>
+        </div>
+        <div className="ExtraInfo row">
+          <section class="weather-details col">
+            <ul>
+              <li>
+                <span class="extra-details"> Wind: 5 km/h</span>
+              </li>
+              <li>
+                <span class="extra-details"> Humidity: 46%</span>
+              </li>
+              <li>
+                <span class="extra-details"> Feels like: 17 °C</span>
+              </li>
+            </ul>
+          </section>
+          <div className="main-symbol col">
+            <p>🌤️</p>
+          </div>
         </div>
       </div>
-      <div className="ExtraInfo row">
-        <section class="weather-details col">
-          <ul>
-            <li>
-              <span class="extra-details"> Wind: 5 km/h</span>
-            </li>
-            <li>
-              <span class="extra-details"> Humidity: 46%</span>
-            </li>
-            <li>
-              <span class="extra-details"> Feels like: 17 °C</span>
-            </li>
-          </ul>
-        </section>
-        <div className="main-symbol col">
-          <p>🌤️</p>
-        </div>
-      </div>
-    </div>
-  );
+    );
+  } else {
+    let city = "Prague";
+    const apiKey = "t8c4bc88f33a8fff2c5o00a1f6b0d692";
+    let url = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+
+    return "Loading...";
+  }
 }
