0844c5e508965b1a357c0a87027836309f4a9a63
LindaSz0112 <lindaszarvas@gmail.com>
2023-06-08 17:35:59 +0200

    Fixing the default page and search

--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -3,9 +3,9 @@ import "./WeatherApp.css";
 import FriendlyDate from "./FriendlyDate";
 import axios from "axios";
 
-export default function WeatherApp() {
+export default function WeatherApp(props) {
   const [loaded, setLoaded] = useState(false);
-  const [city, fetchCity] = useState(" ");
+  const [city, fetchCity] = useState("Prague");
   const [weather, setWeather] = useState({});
   const [displayedCity, setDisplayedCity] = useState("");
 
@@ -13,11 +13,15 @@ export default function WeatherApp() {
     fetchCity(event.target.value);
   }
 
-  function handleSubmit(event) {
-    event.preventDefault();
+  function search() {
     const apiKey = "t8c4bc88f33a8fff2c5o00a1f6b0d692";
     let url = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
     axios.get(url).then(setDetails);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
   }
 
   function setDetails(response) {
@@ -95,49 +99,7 @@ export default function WeatherApp() {
       </div>
     );
   } else {
-    return (
-      <div className="WeatherApp">
-        <form className="Search">
-          <input type="text" placeholder="City" onChange={setCity} />
-          <button type="submit" onClick={handleSubmit}>
-            Search
-          </button>
-        </form>
-        <div className="CityDetails row">
-          <div className="city-details col">
-            <h1>Prague</h1>
-            <span>11.05.2023.</span>
-            <br />
-            <span>17:38</span>
-          </div>
-          <div className="temperature col">
-            <h2>
-              <span className="temperature">18</span>
-              <span className="temp-c">°C </span>
-
-              <div>Clear Sky</div>
-            </h2>
-          </div>
-        </div>
-        <div className="ExtraInfo row">
-          <section class="weather-details col">
-            <ul>
-              <li>
-                <span class="extra-details"> Wind: 5 km/h</span>
-              </li>
-              <li>
-                <span class="extra-details"> Humidity: 46%</span>
-              </li>
-              <li>
-                <span class="extra-details"> Feels like: 17 °C</span>
-              </li>
-            </ul>
-          </section>
-          <div className="main-symbol col">
-            <p>🌤️</p>
-          </div>
-        </div>
-      </div>
-    );
+    search();
+    return "Loading...";
   }
 }
